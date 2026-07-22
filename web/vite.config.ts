647bd198f804ef88058d2786812d5fee829df413
zxcvkim <190175095+asdfkim@users.noreply.github.com>
2026-07-22 20:15:33 +0900

    ✨ (frontend) Initialize (SvelteKit)

---
+++ web/vite.config.ts
@@ -0,0 +1,29 @@
+import tailwindcss from '@tailwindcss/vite';
+import adapter from '@sveltejs/adapter-static';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+		sveltekit({
+			compilerOptions: {
+				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+				runes: ({ filename }) =>
+					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+			},
+			adapter: adapter({
+        pages: 'dist',
+        assets: 'dist',
+        fallback: 'index.html',
+        precompress: false,
+        strict: true
+			})
+		})
+  ],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  }
+});
